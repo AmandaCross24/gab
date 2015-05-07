bba36ceb2f64bae03ba9d88a8f585ea0eea534fe
sendeavors <bermpluviose@gmail.com>
2015-05-07 12:36:24 -0400

    Updated main.rb and home.erb

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-# gab
+# Gab
 
-This is our microblog.  In this application, the user will be able to sign-up, sign-in, log out, delete his account, post blog entries limited to 150 characters, edit entries, and follow other users.  
+This is our microblog called Gab.  In this application, users are able to 
 
+* sign up
+* sign in
+* sign out
+* edit their account details
+* delete their account
+* post micro-blogs (posts with less than 150 characters)
+* see a profile page with all of their account details and micro-blog posts to date
+* visit other users' profile pages
+* see a page with the 10 most recent posts (the “feed”)
+* follow other users
+* see a list of users they follow
--- main.rb
+++ main.rb
@@ -14,16 +14,16 @@ set :sessions, true
 use Rack::Flash, sweep: true
 
 
-def current_user
-	if session[:user_id]
-		User.find(session[:user_id]) ? nil
-	end
-end
+# def current_user
+# 	if session[:user_id]
+# 		User.find(session[:user_id]) : nil
+# 	end
+# end
 
 # Ternary Operator
-# def current_user
-# 	session[:user_id] ? User.find(session[:user_id]) : nil
-#  end
+def current_user
+	session[:user_id] ? User.find(session[:user_id]) : nil
+end
 
 get '/' do
 	erb :home
@@ -35,8 +35,27 @@ post '/login' do
 		session[:user_id] = my_user.id
 		redirect to ('/user')
 	else
-		flash[:notice] = "Try Again"
+		redirect to ('/signup')
 	end
+end
+
+#Attempt to loop login 3 times before redirecting to signup page
+# post '/login' do
+# 	i = 0
+# 	while i < 3
+# 		my_user = User.find_by email: params[:email]
+# 		if my_user and my_user.password == params[:password]
+# 			session[:user_id] = my_user.id
+# 			redirect to ('/user')
+# 		else
+# 			
+# 		end
+# 	end
+#		redirect to ('/signup')
+# end
+
+get '/user' do
+	erb :user
 end
 
 get '/signup' do
@@ -50,12 +69,13 @@ post '/signup' do
 	redirect to ('/user')
 end
 
-get '/user' do
-	erb :user
-end
-
 post '/logout' do
 	session[:user_id] = nil
 	"You have been logged out"
 end
+
+# <% @users.each do |user| %>   Name: <%= user.fname %> <% end %>
+
+
+
 
